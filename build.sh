386e18d2677260b16b35ccb4c5cb83709fea228e
Jeremy Leipzig <leipzig@gmail.com>
2022-03-14 22:29:30 -0600

    bdcli build

---
+++ build.sh
@@ -0,0 +1,4 @@
+docker build -t truwl/nfcore-hlatyping:1.2.0_0.1.0 -t truwl/nfcore-hlatyping:latest .
+docker push truwl/nfcore-hlatyping:1.2.0_0.1.0
+docker push truwl/nfcore-hlatyping:latest
+        
